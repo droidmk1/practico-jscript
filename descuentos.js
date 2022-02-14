e3785df1d88fc1edfba4662182cc6c03bee40810
Alvaro Farfan <alvaro_farfan1@hotmail.com>
2022-02-14 11:59:55 -0500

    Se agrego el html faltante

---
+++ descuentos.js
@@ -0,0 +1,28 @@
+// const precioOriginal = 120;
+// const descuento = 18;
+function calcularPrecioConDescuento(precio, descuento) {
+	const porcentajePrecioConDescuento = 100 - descuento;
+	const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+	return precioConDescuento;
+}
+
+// console.log({
+// 	precioOriginal,
+// 	descuento,
+// 	porcentajePrecioConDescuento,
+// 	precioConDescuento,
+// });
+
+function onCliclButtonPriceDiscount() {
+	const inputPrice = document.getElementById("inputPrice")
+	const priceValue = inputPrice.value;
+
+	const inputDiscount = document.getElementById("inputDiscount")
+	const discountValue = inputDiscount.value;
+
+	const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+	const resultP = document.getElementById("ResultP")
+	resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
